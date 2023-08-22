7a12964b325a411b6cdc005c5d16007a7b386e01
juasmis <juan11iguel@gmail.com>
2023-08-22 22:06:58 +0200

    Trying to get the package to  work

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='librescada_utils',
-    version='0.1',  # Update with your desired version
+    version='0.2',  # Update with your desired version
     packages=find_packages(),
     install_requires=[
         # List your dependencies here
